85bfa552b05adc113185673cc9a86b905012c6a2
KaranKharb <120930624+KaranKharb@users.noreply.github.com>
2022-12-22 20:38:41 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+// database configuration
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'login');
+$conn = mysqli_connect(DB_SERVER , DB_USERNAME , DB_PASSWORD , DB_NAME);
+// checking connection
+if($conn == false){
+    dir('Error: Cannot Connect');
+}
+?>
